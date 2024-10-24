ea5412aec0159a020d73e9fb40f1acd2dd0fcf53
kithmina1999 <124335388+kithmina1999@users.noreply.github.com>
2024-10-24 22:40:19 +0530

    complete all section and implemet animation for each section using framer motion

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,55 @@
+'use client';
+import ArrowRight from '@/assets/arrow-right.svg'
+import starImage from '@/assets/star.png'
+import springImage from '@/assets/spring.png'
+import Image from 'next/image';
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { useRef } from 'react';
+
 export const CallToAction = () => {
-  return null;
+  const sectionRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['start end', 'end start']
+  })
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+  return <section ref={sectionRef} className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+    <div className="container">
+      <div className='section-heading relative'>
+        <h2 className="section-title">Sign up for free today</h2>
+        <p className="section-description mt-5">
+          Join thousands of users who are already boosting their productivity. Sign up now and get instant access to all our tools, completely free, with no credit card required.
+        </p>
+
+        <motion.img
+          style={{
+            translateY
+          }}
+          src={starImage.src}
+          alt='star image'
+          width={360}
+          className='absolute -left-[350px] -top-[137px]'
+        />
+        <motion.img
+          style={{
+            translateY
+          }}
+          src={springImage.src}
+          alt='spring image'
+          width={360}
+          className='absolute -right-[331px] -top-[19px]'
+        />
+        
+      </div>
+
+      <div className=" flex gap-2 mt-10 justify-center">
+        <button className="btn btn-primary">Get for free</button>
+        <button className="btn btn-text gap-1">
+          <span>Learn more</span>
+          <ArrowRight className='w-5  h-5' />
+        </button>
+      </div>
+    </div>
+  </section>;
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,58 @@
+'use client'
+import productImage from '@/assets/product-image.png'
+import pyramidImage from '@/assets/pyramid.png'
+import tubeImage from '@/assets/tube.png'
+import {motion,useScroll, useTransform} from 'framer-motion'
+
+import Image from 'next/image';
+import { useRef } from 'react'
 export const ProductShowcase = () => {
-  return null;
+  const sectionRef = useRef(null)
+  const {scrollYProgress} =useScroll({
+    target:sectionRef,
+    offset:['start end','end start']
+  });
+  const translateY = useTransform(scrollYProgress,[0,1],[150,-150])
+
+  return (
+    <section ref={sectionRef} className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
+      <div className="container">
+        <div className='section-heading'>
+          <div className='flex justify-center'>
+            <div className='tag'>Boost your productivity</div>
+          </div>
+          <h2 className='section-title  mt-5'>A more effective way to task progress</h2>
+          <p className='section-description mt-5'>
+            Streamline your workflow and keep track of tasks more efficiently with our innovative tools. Stay organized, meet deadlines, and boost your productivity with features designed to help you succeed every step of the way.
+          </p>
+        </div>
+        <div className='relative'>
+          <Image src={productImage} alt='product image' className='mt-10' />
+
+          <motion.img 
+          style={{
+            translateY,
+          }}
+          src={pyramidImage.src} 
+          alt='pyramid image' 
+          height={262} 
+          width={262} 
+          className='hidden md:block absolute -right-36 -top-32'
+          />
+
+          <motion.img 
+          style={{
+            translateY,
+          }}
+          src={tubeImage.src} 
+          alt='tube image' 
+          height={248} 
+          width={248} 
+          className='hidden md:block absolute bottom-24 -left-36'
+          />
+          
+        </div>
+      </div>
+    </section>
+  );
 };
